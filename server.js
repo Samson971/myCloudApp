edf07c0be76a59f030319018bfc64bc24a0b5b9a
samson971 <samson971@orange.fr>
2019-12-15 18:25:08 +0100

    update

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 var mongodb = require('mongodb');
 var swig = require('swig');
 var mongoClient = mongodb.MongoClient;
-const mustacheExpress = require('mustache-express');
 
 // Constants
 const PORT = 8080;
@@ -15,14 +14,13 @@ var uri = "mongodb://localhost:27017";
 
 // App
 const app = express();
-//app.engine('mustache', mustacheExpress());
-//app.set('view engine', 'mustache');
-//app.set('view engine', 'twig');
-var swig = new swig.Swig();
+swig = new swig.Swig();
 app.engine('html', swig.renderFile);
 app.set('view engine', 'html');
 //app.use(express.static(path.join(__dirname, 'bootstrap')));
 
+
+app.use(express.static('public'))
 
 //some useful function
 
@@ -32,7 +30,7 @@ app.set('view engine', 'html');
  * Routes
  */
 app.get('/', (req, res) => {
-    res.send('Hello world');
+    return res.render('home');
 });
 
 app.get('/test', (req, res) => {
@@ -44,7 +42,7 @@ app.get('/test', (req, res) => {
                 res.send(err);
                 return;
             }
-            db = client.db(DATABASE);
+            const db = client.db(DATABASE);
             const collection = db.collection(COLLECTION);
             collection.countDocuments((error, count) => {
                 if (error) {
@@ -60,6 +58,7 @@ app.get('/test', (req, res) => {
 
 
 app.get('/get', (req, res) => {
+    var fieldsToRender = ['Name','Rank']
     mongoClient.connect(uri,
         { useUnifiedTopology: true },
         (err, client) => {
@@ -68,19 +67,22 @@ app.get('/get', (req, res) => {
                 res.send(err);
                 return;
             }
-            db = client.db('imdb');
+            const db = client.db('imdb');
             const collection = db.collection('imdb_denormalized');
-            collection.find({ "Directors.Lastname": "Lucas", "Rank": { "$gt": 0.0 } }).project({ "Name": 1, "Rank": 1 }).toArray((error, docs) => {
+            collection.find({ "Directors.Lastname": "Lucas", "Rank": { "$gt": 0.0 } })
+            .sort({"Rank":-1})
+            .toArray((error, docs) => {
                 if (error) {
                     return res.send(error);
                 }
                 return res.render('basic-table',
-                    { elements: docs });
+                    { elements: docs, fields: fieldsToRender });
             });
         });
 });
 
-app.get('/request/:requestID', (req, res) => {
+app.get('/request/1', (req, res) => {
+    var fieldsToRender = ['Name','Rank','Directors.Firstname','Directors.Lastname']
     mongoClient.connect(uri,
         { useUnifiedTopology: true },
         (err, client) => {
@@ -89,23 +91,86 @@ app.get('/request/:requestID', (req, res) => {
                 res.send(err);
                 return;
             }
-            db = client.db('imdb');
+            const db = client.db('imdb');
             const collection = db.collection('imdb_denormalized');
             //execute request
-            opMatch = {$match:{"Rank":{$ne:0.0,$lte:50}}};
-            opUnwind = {$unwind:"$Directors"};
-            opGroup = {$group:{_id:"$Directors","avg_notes":{$avg:"$Rank"},"nb":{$sum:1}}};
-            opSort = {$sort:{"avg_notes":-1}};
-            opLimit = { $limit: 1 };
-            collection.aggregate([opMatch, opUnwind, opGroup, opSort, opLimit]).toArray((error, docs) => {
+            collection.find({"Directors.Firstname":"Sara","Directors.Lastname":"Botsford"})
+            .toArray((error, docs) => {
                 if (error) {
                     return res.send(error);
                 }
                 return res.render('basic-table',
-                    { elements: docs });
+                    { 
+                        elements: docs, 
+                        fields: fieldsToRender 
+                    });
             });
         });
 });
+
+
+app.get('/request/2', (req, res) => {
+    var fieldsToRender = ['Name','Rank','Firstname','Lastname']
+    mongoClient.connect(uri,
+        { useUnifiedTopology: true },
+        (err, client) => {
+            if (err) {
+                console.error(err);
+                res.send(err);
+                return;
+            }
+            const db = client.db('imdb');
+            const collection = db.collection('imdb_denormalized');
+            //execute request
+            const opMatch = {'$match':{"Rank":{'$ne':0.0,'$lte':50}}};
+            const opUnwind = {'$unwind':"$Directors"};
+            const opGroup ={'$group':{'_id':"$Directors","avg_notes":{'$avg':"$Rank"},"nb":{'$sum':1}}};
+            const opSort = {'$sort':{"avg_notes":-1}};
+            const opLimit = { '$limit': 10 };
+            collection.aggregate([opMatch, opUnwind, opGroup,opLimit]).toArray((error, docs) => {
+                if (error) {
+                    return res.send(error);
+                }
+                return res.render('basic-table',
+                    { 
+                        elements: docs, 
+                        fields: fieldsToRender 
+                    });
+            });
+        });
+});
+
+app.get('/request/3', (req, res) => {
+    var fieldsToRender = ['Name','Rank','Directors.Firstname','Directors.Lastname']
+    var genre = 'Action';
+    var proba = 90;
+    mongoClient.connect(uri,
+        { useUnifiedTopology: true },
+        (err, client) => {
+            if (err) {
+                console.error(err);
+                res.send(err);
+                return;
+            }
+            const db = client.db('imdb');
+            const collection = db.collection('imdb_denormalized');
+            //execute request
+            const opUnwind = {'$unwind':"$Directors"};
+            const opMatch = {'$match':{}}
+            collection.find({"Directors.Firstname":"Sara","Directors.Lastname":"Botsford"})
+            .toArray((error, docs) => {
+                if (error) {
+                    return res.send(error);
+                }
+                return res.render('basic-table',
+                    { 
+                        elements: docs, 
+                        fields: fieldsToRender 
+                    });
+            });
+        });
+});
+
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
